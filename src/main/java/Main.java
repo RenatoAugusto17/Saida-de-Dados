c0b1b39df720d00bc2320502b4a8f41e13aba7de
RenatoAugusto17 <169068426+RenatoAugusto17@users.noreply.github.com>
2024-06-05 11:47:07 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+Scanner SC = new Scanner(System.in);
+
+ //   System.out.println("Entre com um número inteiro: ");
+   // int inteiro = SC.nextInt();
+   // System.out.println("O número digitado foi");
+ //   System.out.println(inteiro);
+
+ //System.out.println("Entre com um número racional: ");
+ //   double racional = SC.nextDouble();
+ //   System.out.println("O número digitado foi");
+ //   System.out.println(racional);
+
+    System.out.println("Entre com o nome de seu futuro filho(a): ");
+    double racional = SC.nextDouble();
+    System.out.println("O número digitado foi");
+    System.out.println(racional);
+    
+    SC.close();
+  }
+}
